94e68a6208860aee78e04f37c8a834e1104d9b19
Miguel D. Salcedo <miguel@salcedo.cc>
2024-07-15 00:58:05 -0400

    fix condition on join loop

--- cmd/example/cluster.go
+++ cmd/example/cluster.go
@@ -53,10 +53,7 @@ func (c *ClusterDelegate) FastJoin(cluster string) error {
 func (c *ClusterDelegate) Join(ctx context.Context, cluster string) error {
 	start := time.Now()
 
-	var err error
-	var peers []string
-
-	for peers, err = c.fetchPeers(cluster); c.cluster.NumMembers() > 1 && len(peers) == 0 && err == nil; peers, err = c.fetchPeers(cluster) {
+	for peers, err := c.fetchPeers(cluster); c.cluster.NumMembers() <= 1 || len(peers) > 0 || err != nil; peers, err = c.fetchPeers(cluster) {
 		select {
 		case <-ctx.Done():
 			return err
